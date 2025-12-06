05515d3325a5e2df99232520f1ab203f51b7b449
Dimas-Eko <dhimasprasetyo0@gmail.com>
2025-12-06 21:08:26 +0700

    feat: Get Stok By Barang

--- warehouse-api/handlers/stok_handler.go
+++ warehouse-api/handlers/stok_handler.go
@@ -2,10 +2,10 @@ package handlers
 
 import (
 	"net/http"
-
+	"strconv"
 	"warehouse-api/repositories"
-
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 )
 
 type StokHandler struct {
@@ -37,3 +37,46 @@ func (h *StokHandler) GetAllStok(c *gin.Context) {
 		},
 	})
 }
+
+
+func (h *StokHandler) GetStokByBarang(c *gin.Context) {
+	barangIDParam := c.Param("barang_id")
+
+
+	idInt, err := strconv.Atoi(barangIDParam)
+	if err != nil || idInt < 1 {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"success": false,
+			"message": "Invalid barang_id parameter",
+			"data":    nil,
+		})
+		return
+	}
+	barangID := uint(idInt)
+
+	stok, err := h.Repo.GetStokByBarangID(barangID)
+	if err != nil {
+		if err == gorm.ErrRecordNotFound {
+			c.JSON(http.StatusNotFound, gin.H{
+				"success": false,
+				"message": "Stok untuk barang ini tidak ditemukan",
+				"data":    nil,
+			})
+			return
+		}
+
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"success": false,
+			"message": "Failed to get stok data",
+			"data":    nil,
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"success": true,
+		"message": "Data retrieved successfully",
+		"data":    stok,
+	})
+}
+
